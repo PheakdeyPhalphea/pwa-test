13b1b6b84c609a2acd8a939072b584afffb19fe5
kdey <kdeylester@gmail.com>
2025-02-20 11:52:11 +0700

    version2

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable PWA in dev mode
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
   experimental: {
-    // Remove turbopack for now
-    // turbopack: true,
+   
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,63 @@
 "use client";
 
-import InstallPWAAlert from "@/beforeinstallprompt ";
+import { useEffect, useState } from "react";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
 
 export default function Home() {
+  const [deferredPrompt, setDeferredPrompt] =
+    useState<BeforeInstallPromptEvent | null>(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event: BeforeInstallPromptEvent) => {
+      event.preventDefault(); // Prevent the default install prompt
+      setDeferredPrompt(event); // Store the event to trigger it later
+    };
+
+    window.addEventListener(
+      "beforeinstallprompt",
+      handleBeforeInstallPrompt as EventListener
+    );
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt as EventListener
+      );
+    };
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const choiceResult = await deferredPrompt.userChoice;
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the PWA installation");
+      } else {
+        console.log("User dismissed the PWA installation");
+      }
+      setDeferredPrompt(null); // Clear the deferred prompt event
+    } else {
+      alert("Installation is not available.");
+    }
+  };
   return (
-    <div>
-      <InstallPWAAlert />
+    <div className="w-full mx-auto text-center items-center h-screen flex flex-col  justify-center">
+      <p className="text-2xl my-10">Testing PWA</p>
+      <button
+        className="p-[10px] bg-blue-700 text-white rounded-lg"
+        onClick={handleInstallClick}
+      >
+        Install App
+      </button>
     </div>
   );
 }
